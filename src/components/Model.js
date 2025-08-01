75e190ae9329915f8d9e013c0cf99d78e6af2245
Reem Abdelkader <96788888+ReemAbdelkader@users.noreply.github.com>
2025-08-01 05:16:13 +0300

    Add files via upload

---
+++ src/components/Model.js
@@ -0,0 +1,11 @@
+import './FormStyle.css';
+
+export default function Model() {
+    return (
+        <div className='mainModel'>
+            <div className='modelStyle'>
+                <h1>The Form Has Been Submitted Successfully</h1>
+            </div>
+        </div>
+    )
+}
